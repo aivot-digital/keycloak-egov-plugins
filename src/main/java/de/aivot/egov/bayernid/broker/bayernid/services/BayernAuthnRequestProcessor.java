cc48adc8b909fa121019df66656d118cf3b1f0ba
Finn Moritz Bätzel <code@mobaetzel.de>
2024-03-03 14:00:00 +0100

    Fix BayernID/BundID Level

--- src/main/java/de/aivot/egov/bayernid/broker/bayernid/services/BayernAuthnRequestProcessor.java
+++ src/main/java/de/aivot/egov/bayernid/broker/bayernid/services/BayernAuthnRequestProcessor.java
@@ -3,22 +3,26 @@ package de.aivot.egov.bayernid.broker.bayernid.services;
 import de.aivot.egov.bayernid.broker.bayernid.enums.BayernIdAccessLevel;
 import de.aivot.egov.bayernid.broker.bayernid.generators.BayernIdAuthenticationRequestExtensionGenerator;
 import org.jboss.logging.Logger;
+import org.keycloak.OAuth2Constants;
 import org.keycloak.dom.saml.v2.protocol.AuthnContextComparisonType;
 import org.keycloak.dom.saml.v2.protocol.AuthnRequestType;
 import org.keycloak.dom.saml.v2.protocol.ExtensionsType;
 import org.keycloak.dom.saml.v2.protocol.RequestedAuthnContextType;
 import org.keycloak.sessions.AuthenticationSessionModel;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class BayernAuthnRequestProcessor {
     private final static Logger logger = Logger.getLogger(BayernAuthnRequestProcessor.class);
 
     public AuthnRequestType processBeforeSendingLoginRequest(AuthnRequestType authnRequest, AuthenticationSessionModel clientSession) {
-        var accessLevel = clientSession
-                .getClient()
-                .getClientScopes(true)
-                .keySet()
+        var scopeParam = clientSession.getClientNote(OAuth2Constants.SCOPE);
+        var scopes = scopeParam != null ? Arrays.asList(scopeParam.split("\\s+")) : new LinkedList<String>();
+
+        var accessLevel = scopes
                 .stream()
                 .peek(scopeKey -> logger.info("Checking Scope: " + scopeKey))
                 .map(BayernIdAccessLevel::fromScopeValue)
--- src/main/java/de/aivot/egov/bundid/broker/bundid/services/BundIdAuthnRequestProcessor.java
+++ src/main/java/de/aivot/egov/bundid/broker/bundid/services/BundIdAuthnRequestProcessor.java
@@ -3,22 +3,25 @@ package de.aivot.egov.bundid.broker.bundid.services;
 import de.aivot.egov.bundid.broker.bundid.enums.BundIdAccessLevel;
 import de.aivot.egov.bundid.broker.bundid.generators.BundIdAuthenticationRequestExtensionGenerator;
 import org.jboss.logging.Logger;
+import org.keycloak.OAuth2Constants;
 import org.keycloak.dom.saml.v2.protocol.AuthnContextComparisonType;
 import org.keycloak.dom.saml.v2.protocol.AuthnRequestType;
 import org.keycloak.dom.saml.v2.protocol.ExtensionsType;
 import org.keycloak.dom.saml.v2.protocol.RequestedAuthnContextType;
 import org.keycloak.sessions.AuthenticationSessionModel;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Optional;
 
 public class BundIdAuthnRequestProcessor {
     private final static Logger logger = Logger.getLogger(BundIdAuthnRequestProcessor.class);
 
     public AuthnRequestType processBeforeSendingLoginRequest(AuthnRequestType authnRequest, AuthenticationSessionModel clientSession) {
-        var accessLevel = clientSession
-                .getClient()
-                .getClientScopes(true)
-                .keySet()
+        var scopeParam = clientSession.getClientNote(OAuth2Constants.SCOPE);
+        var scopes = scopeParam != null ? Arrays.asList(scopeParam.split("\\s+")) : new LinkedList<String>();
+
+        var accessLevel = scopes
                 .stream()
                 .peek(scopeKey -> logger.info("Checking Scope: " + scopeKey))
                 .map(BundIdAccessLevel::fromScopeValue)
